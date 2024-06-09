7c92444b9892539160d5b1930ceb7227eccf9df9
lolxlolnk <lolilolshindo@gmail.com>
2024-06-09 20:14:28 +0200

    Update deleteWebhook.py

--- src/utils/deleteWebhook.py
+++ src/utils/deleteWebhook.py
@@ -2,7 +2,7 @@ import requests
 from utils.common import *
 
 def deleteWebhook(url):
-    set_console_title("Chronos V1 | Made by maxツ#8355 | Delete Webhook")
+    set_console_title("lolxlol | Delete Webhook")
     requests.delete(url)
     print(f"{Fore.WHITE}[{Fore.LIGHTCYAN_EX}C{Fore.WHITE}] Deleted Webhook")
     sleep(1)
